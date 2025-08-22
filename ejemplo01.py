6770775cad6bdd8658dfdc0b9ce7182715319b6b
Gina Maza <f0257123@pucp.edu.pe>
2025-08-21 20:01:52 -0500

    se agrego funcion de potencia

--- ejemplo01.py
+++ ejemplo01.py
@@ -1,9 +1,14 @@
 def sumar_10(x):
     return x+10
 
+def potencia(x):
+    return x*x
+
 print("Ejemplo de codigo python")
 x= 5+6
 print(x)
+x=44
 nombre='Gina Maza'
-print(nombre)
-print(sumar_10(44))
+print('Nombre: ', nombre)
+print('edad: ', sumar_10(x))
+print('Potencia', potencia(x))
